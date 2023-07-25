ac7241f0fa3cfe05d85227cd38a5d0ef52b26bdc
Celestin Gabor <127986056+celegabor@users.noreply.github.com>
2023-07-25 21:36:08 +0200

    aggiunta dati alla card

--- main.js
+++ main.js
@@ -4,16 +4,34 @@ const apiKey = 'ZKA03SJniHXA8Ai2IrlbfrF8DNweDiXZdPONMAiWMR6vMM1IkeNILx3p';
 
 let result = document.querySelector('#result')
 
-function createCard(imageUrl) {
+function createCard(imageUrl, imageId, widthValue, heightValue, photographerUrl) {
     const card = document.createElement('div');
     card.className = 'card';
 
     const image = document.createElement('img');
     image.src = imageUrl;
 
+    const widthElement = document.createElement('p');
+    widthElement.innerText = 'WIDTH: ' + widthValue; 
+
+    const heightElement = document.createElement('p');
+    heightElement.innerText = 'HEIGHT: ' + heightValue; 
+
+    const idElement = document.createElement('p');
+    idElement.innerText = 'ID: ' + imageId;
+
+    const photographerUrlElement = document.createElement('p');
+    photographerUrlElement.innerHTML = 'Photographer URL: <a href="' + photographerUrl + '">' + photographerUrl + '</a>';
+
     card.appendChild(image);
+    card.appendChild(idElement);
+    card.appendChild(widthElement);
+    card.appendChild(heightElement);
+    card.appendChild(photographerUrlElement);
+    
     return card;
 }
+
 
 function sito() {
 
@@ -36,7 +54,12 @@ function sito() {
     
     images.forEach(photo => {
         const imageUrl = photo.src.small;
-        const card = createCard(imageUrl);
+        const imageId = photo.id;
+        const widthValue = photo.width;
+        const heightValue = photo.height;
+        const photographerUrl = photo.photographer_url;
+    
+        const card = createCard(imageUrl, imageId, widthValue, heightValue, photographerUrl);
         cardsContainer.appendChild(card);
     });
     
@@ -44,7 +67,5 @@ function sito() {
 })
   .catch(err => console.log('risp neg: ', err));
 }
-
-
 
 sito()
