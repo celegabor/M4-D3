2603de65e2d977c6b0e128e2232dc294b1d15302
Celestin Gabor <127986056+celegabor@users.noreply.github.com>
2023-07-25 20:52:47 +0200

    ritorno immagini

--- main.js
+++ main.js
@@ -1,12 +1,50 @@
 
 const apiKey = 'ZKA03SJniHXA8Ai2IrlbfrF8DNweDiXZdPONMAiWMR6vMM1IkeNILx3p';
 
-function sito() {
-    return fetch('https://api.pexels.com/v1/',{
-    headers: {
-        'Authorization' : apiKey
-    }
-})
+
+let result = document.querySelector('#result')
+
+function createCard(imageUrl) {
+    const card = document.createElement('div');
+    card.className = 'card';
+
+    const image = document.createElement('img');
+    image.src = imageUrl;
+
+    card.appendChild(image);
+    return card;
 }
 
-sito().then(res => console.log('risposta: ', res)).catch(err => console.log('risp neg: ', err)).finally(() => console.log('fine'));
+function sito() {
+
+    let inserimento = document.querySelector('#inserimento').value;
+
+  return fetch('https://api.pexels.com/v1/search?query=' + inserimento, {
+    headers: {
+      'Authorization': apiKey
+    }
+  })
+  .then(res => res.json())
+  .then(res => {
+    console.log(res);
+    const images = res.photos;
+    
+    
+    const cardsContainer = document.getElementById('cardsContainer');
+    cardsContainer.innerHTML = '';
+    
+    
+    images.forEach(photo => {
+        const imageUrl = photo.src.small;
+        const card = createCard(imageUrl);
+        cardsContainer.appendChild(card);
+    });
+    
+    result.innerText = 'La tua ricerca: ' + inserimento.toUpperCase() + ' ha generato: ' + images.length + ' risultati';
+})
+  .catch(err => console.log('risp neg: ', err));
+}
+
+
+
+sito()
